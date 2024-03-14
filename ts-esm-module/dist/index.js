114000982eb63eb3f447bff1f785dc4e1591af6c
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-03-14 20:05:33 +0900

    commonjsとしてバンドルするよう実装

---
+++ ts-esm-module/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const hoge_1 = require("./hoge");
+const array = ["a", "b", "c"];
+console.log(array.at(-1), hoge_1.hoge);
---
+++ ts-esm-module/index.ts
@@ -0,0 +1,4 @@
+import { hoge } from "./hoge";
+const array = ["a", "b", "c"] as const;
+
+console.log(array.at(-1), hoge);

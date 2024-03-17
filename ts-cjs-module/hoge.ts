8026f68a13297410180b07e74a2493116b6627c4
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-03-17 16:53:48 +0900

    型は出力されないことの確認

--- ts-cjs-module/hoge.ts
+++ ts-cjs-module/hoge.ts
@@ -1,1 +1,2 @@
 export const hoge = "hoge";
+export type Strings = string[];
--- ts-cjs-module/index.ts
+++ ts-cjs-module/index.ts
@@ -1,4 +1,4 @@
-import { hoge } from "./hoge";
-const array = ["a", "b", "c"] as const;
+import { hoge, Strings } from "./hoge";
+const array = ["a", "b", "c"] as const satisfies Strings;
 
 console.log(array.at(-1), hoge);
